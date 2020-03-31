5bf880bd8da6a0f72f9033831bfcd2c21e19f0d6
Jai Kumar Singh <jaikumarsingh1908@gmail.com>
2020-03-31 18:33:15 +0530

    Objects in ES6

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
-//string interpolation
-//In ES5
-var x=10;
-var name="Jai";
-var mess="My name is "+name+" and age is "+x;
-console.log(mess);
-//in ES6
-var mess1=`My name is ${name} and age is ${x}`;
-console.log(mess1);
-//Multiline in ES5
-var m="Hi i am Jai\n"+
-"and my age is 21";
-console.log(m);
-//Multiline in ES6
-var m1=`Hi my name is
-Jai and my age is 21`;
-console.log(m1);
-//Tag
-function tag(strings,...values)
+//Objects in ES6
+key="lastname";
+var person=
 {
-    console.log(strings);
-    console.log(values);
-}
-const name1="Jai";
-const age=21;
-tag`Hello my name is ${name1} and my age is ${age}`;
+    "fname":"Jai",
+    [key]:"Singh",
+    ['age'] :21,
+    birth ()
+    {
+        this.age++;
+    }
+};
+person;
