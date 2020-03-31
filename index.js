bface4090b5efba66ed8fdb45b7f181af77694d3
Jai Kumar Singh <jaikumarsingh1908@gmail.com>
2020-03-31 15:57:31 +0530

    Default parameter in ES6

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-const arr=[1,2,3,5,6];
-const bytwo=arr.map(function(number)
+//E5 logic
+function buy(item,amount)
 {
-    return number*2;
-});
-const bythree=arr.map((number) =>
-{
-    return number*3;
-});//statement body
-const byfour=arr.map(number => number*4);//expression body
-//Return an object
-const byfive=arr.map((number) =>
-(
+    if (amount === undefined)
     {
-        number:number*5
+        amount =1;
     }
-) 
-);
-//convert every number to 6
-const bysix=arr.map(() => 6);
-arr;
-bytwo;
-bythree;
-byfour;
-byfive;
-bysix;
+    amount;
+    item;
+}
+function buys(item="Choco",amount=3)//we can pass obj ,array etc.
+{
+    amount;
+    item;
+}
+buy("eggs");
+buys();
